156b568ea3a58c227eccf5a311d19a11bb79c934
Tessie Akin <tolulopeakinwale955@gmail.com>
2025-03-10 14:02:25 +0100

    todays commit

--- Number-guess.js
+++ Number-guess.js
@@ -1,1 +1,38 @@
-// closed for the day continue tommorrow
+//NUMBER GUESSING GAME
+
+// this is the equation you need to generate a random number
+const minNum = 1; 
+const maxNum = 100; 
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+console.log(answer)
+
+// lets create a variable to keep track of the attempts the user made
+
+let attempt = 0; // this is the number of attempts the user made
+let guess; // this is the number the user guessed
+let running = true;
+
+while(running){ // while running is true
+guess = window.prompt(`guess a number between ${minNum} - ${maxNum}`)
+guess = Number(guess)
+if(isNaN(guess)){// if the guess is not a number
+    window.alert("Please enter a number")
+    }
+    else if(guess < minNum || guess > maxNum){ // if the guess is less than minNum or greater than maxNum
+      window.prompt(`Please enter a valid number`)
+    }
+    else{
+      attempt++; 
+      if(guess < answer){ // if the guess is less than answer
+        window.alert(`too low try again`)
+        }
+        else if (guess > answer){ //if the guess is greater than answer
+          window.alert(`above limit try again`)
+        }
+        else{ // if the guess is equal to the answer
+        window.alert(`CORRECT! the answer was ${answer} and it took you ${attempt} attempts`)
+        running = false; // this will stop the loop
+        // remember in javascript the code runs from top to bottom
+        }
+    }
+}
---
+++ function.js
@@ -0,0 +1,13 @@
+// FUNCTION -  A block of code that can be called
+// it can take in parameters and retun a value
+// template  - function functionName(parameters){ code to be executed}
+
+function Anniversary(username, years){
+  console.log("Happy Anniversary");
+  console.log(`Happy Anniversary ${username} it been ${years} years`);
+}
+Anniversary("victor", 24);// to display the message in your console you need to call the function
+
+function learners(){// we call the function 
+let message = document.getElementById("display").textContent = "Welcome Senior Developers 😊  Keep up the Good work"
+}
